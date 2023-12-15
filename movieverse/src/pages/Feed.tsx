84e2d9e28fce56fa51b84a667a42c89784053fc3
Egemen Akdan <egemenvkdvn35@gmail.com>
2023-12-15 14:45:43 +0300

    recommendation algorithm

--- movieverse/src/pages/Feed.tsx
+++ movieverse/src/pages/Feed.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
+import { useUsers } from "~/hooks/useUsers"
+import { getUserGenreMap, userSpecialMovieDistribution } from "~/utils/helpers";
 
 export const Feed = () => {
+  const {user} = useUsers();
+
+  useEffect(()=>{
+   const genremap = getUserGenreMap(user);
+   const movies = userSpecialMovieDistribution(genremap);
+    console.log(genremap,movies);
+  },[user])
+
   return (
-    <div>Feed</div>
+    <div className="page">
+      <div className="page-header">
+        <h1>For You</h1>
+      </div>
+    </div>
   )
 }
--- movieverse/src/utils/helpers.ts
+++ movieverse/src/utils/helpers.ts
@@ -1,6 +1,9 @@
 import { MoviesFromMovieverse } from "~/types/movies";
 import { User } from "~/types/users";
 
+const SPECIAL_MOVIE_AMOUNT = 10;
+const SPECIAL_OCCURENCE_AMOUNT = 10;
+
 const isNumber = (text:string)=>{
     const isNum = !isNaN(parseInt(text));
     return isNum
@@ -10,4 +13,36 @@ const isFavorited = (title:string,user:User,moviesFromDb:MoviesFromMovieverse[])
     return moviesFromDb.some(movie=>movie.title === title && movie.favorited_from.includes(user.id!)); 
 }
 
-export {isNumber,isFavorited}
+const getUserGenreMap = (user:User) => {
+    const favoriteGenres = user.favoriteGenres;
+   const userGenreMap = favoriteGenres.reduce((pre,curr)=>{
+        if(pre.some(item=> item.genre === curr) ){
+            const index = pre.findIndex(item=> item.genre === curr);
+            const incGenre = pre[index]
+            incGenre.occurence += 1;
+            return pre
+        }else{
+            return [...pre,{genre:curr,occurence:1}]
+        }
+    },[] as {genre:number,occurence:number}[])
+
+    const filteredGenreMap = userGenreMap.filter(item=> item.occurence > SPECIAL_OCCURENCE_AMOUNT);
+
+    const totalOccurency = filteredGenreMap.reduce((total,item)=> total + item.occurence, 0);
+
+    const result = filteredGenreMap.map((item)=>{
+        const percentage = (item.occurence / totalOccurency) * 100;
+        return {...item, percentage:Math.round(percentage)}
+    });
+
+    return result.sort((a,b)=> b.percentage - a.percentage );
+}
+
+const userSpecialMovieDistribution = (genreMap:{genre:number,occurence:number,percentage:number}[]) => {
+    const moviesFrom = genreMap.map((item)=>{
+        const calculatedAmount = (item.percentage * SPECIAL_MOVIE_AMOUNT) / 100;
+        return { genre:item.genre, amount:Math.round(calculatedAmount)}
+    })
+    return moviesFrom
+}
+export {isNumber,isFavorited,getUserGenreMap,userSpecialMovieDistribution}
